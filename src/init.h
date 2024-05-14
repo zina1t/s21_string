67ac9af268a1deee087e4f40b0a5fe36a8d2bf69
zina1t <v.gryazcov1@gmail.com>
2024-05-15 00:42:42 +0300

    2 more funcs

--- src/init.h
+++ src/init.h
@@ -5,7 +5,9 @@
 #define s21_NULL (void *)0
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 s21_size_t s21_strlen(const char *str);
-int s21_strcmp(char *str1, char *str2, s21_size_t n);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strchr(const char *str, int c);
+char *s21_strstr(const char *haystack, const char *needle);
 #endif
