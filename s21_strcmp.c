627a81cb9fe9e91c0865d13557b2ea864a109bf1
zina1t <v.gryazcov1@gmail.com>
2024-08-02 21:35:31 +0300

    removed extra files

--- s21_strcmp.c
+++
@@ -1,9 +0,0 @@
-#include "s21_string.h"
-
-int s21_strcmp(const char *str1, const char *str2) {
-  while (*str1 && (*str1 == *str2)) {
-    str1++;
-    str2++;
-  }
-  return *(unsigned char *)str1 - *(unsigned char *)str2;
-}
